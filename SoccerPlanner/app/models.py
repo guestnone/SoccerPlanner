0120d46bbadee4fa0f292ee003c833d69651dfd5
Patrick Rećko <guest_none@protonmail.com>
2019-06-07 16:07:25 +0200

    WIP: added mail sending via MailJet - fixed error with the model

--- SoccerPlanner/app/models.py
+++ SoccerPlanner/app/models.py
@@ -47,8 +47,8 @@ class Team(models.Model):
 
 class Match(models.Model):
     MatchID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    team1 = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, related_name='team1', null=True,)
-    team2 = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, related_name='team2', null=True,)
+    team1 = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, related_name='team1')
+    team2 = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, related_name='team2')
     points = models.IntegerField(default=0, null=True, blank=True)
     points2 = models.IntegerField(default=0, null=True, blank=True)
     shootersPerMatch = models.ForeignKey(ShootersMatch, on_delete=models.CASCADE, null=True, blank=True)
